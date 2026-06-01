8cc8f178ab34702d19bb8a923c85c5e891f6f8bd
Chris Lee <licshisan@gmail.com>
2026-06-01 11:49:57 +0800

    feat: add prisma db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
@@ -12,7 +11,6 @@ import { PrismaModule } from './prisma/prisma.module';
       envFilePath: ['.env.local', '.env'],
     }),
     PrismaModule,
-    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
