12f32667dcb545a1e0d18b8c730ccc228c36a5fd
Chris Lee <licshisan@gmail.com>
2026-06-01 11:32:57 +0800

    chore: install  plugins

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
+  const port = process.env.PORT ?? 3000;
+  const swaggerPath = 'docs';
+
+  app.use(helmet());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nest Project API')
+    .setDescription('API documentation for Nest Project')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup(swaggerPath, app, document);
+
+  await app.listen(port);
+  logger.log(`Swagger docs available at http://localhost:${port}/${swaggerPath}`);
 }
 bootstrap();
